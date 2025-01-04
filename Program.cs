3ee97302937d73ea024ca8827d27f60f2a2a2a1b
CalvinGaiennie <calvingaiennie@gmail.com>
2025-01-03 21:56:29 -0600

    finished the basic functionality of project with the assistance of cursor. Now i need to spend some time refactoring and understanding it

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProgram
+{
+    public class NewClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] allIngredients = { "sliced bread", "ground beef", "egg", "cheese", "lettuce", "tomato", "onion", "olive oil", "bacon", "butter", "chicken" };
+            string[] breakfastPlate = { "sliced bread", "egg", "cheese", "butter", "bacon" };
+            string[] burger = { "sliced bread", "ground beef", "lettuce", "tomato", "onion", "olive oil" };
+            string[] salad = { "lettuce", "tomato", "onion" };
+            string[] chickenSandwich = { "sliced bread", "chicken", "lettuce", "tomato", };
+
+
+            string ingredientList = string.Join("\n", allIngredients.Select((item, index) => $"{index + 1}. {item}"));
+            Console.WriteLine("Ingredients: \n" + ingredientList);
+
+            Console.Write("list the number of any ingredients you have available: ");
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(',');
+            List<int> inputList = inputArray.Select(int.Parse).ToList();
+            List<string> ingredients = inputList.Select(index => allIngredients[index - 1]).ToList();
+            
+            // Add method to check recipes
+            CheckRecipe(ingredients, breakfastPlate, "Breakfast Plate");
+            CheckRecipe(ingredients, burger, "Burger");
+            CheckRecipe(ingredients, salad, "Salad");
+            CheckRecipe(ingredients, chickenSandwich, "Chicken Sandwich");
+        }
+
+        private static void CheckRecipe(List<string> availableIngredients, string[] recipe, string recipeName)
+        {
+            bool canMake = recipe.All(ingredient => availableIngredients.Contains(ingredient));
+            if (canMake)
+            {
+                Console.WriteLine($"You can make a {recipeName}!");
+            }
+            else
+            {
+                var missingIngredients = recipe.Where(ingredient => !availableIngredients.Contains(ingredient));
+                Console.WriteLine($"You cannot make a {recipeName}. Missing ingredients: {string.Join(", ", missingIngredients)}");
+            }
+        }
+    }
+}
--- obj/Debug/net9.0/MyProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1c0c41dba3ff92697556ca50334b67c630c868")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
