7c0970261645162a4bf669db43c0186bf4930854
CalvinGaiennie <calvingaiennie@gmail.com>
2025-04-28 21:38:30 -0500

    Initiated a full stack project

--- InitPractice/Program.cs
+++ InitPractice/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//I am going to spend the next month working on a full stack project using the Veris stack. I am hoping this will help more learn more about the stack and give me a good project to put on my portfolio.
+
 Console.WriteLine("Hello, World!");
